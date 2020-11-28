9d8f99dcce9f036b476179197853eab5d029dd41
Jeffrey Liu <jeffreyliu66@gmail.com>
2020-11-28 02:14:03 -0800

    httpGetRequest is functioning but matchParser doesn't work

--- api/routes/httpGetRequest.js
+++ api/routes/httpGetRequest.js
@@ -1,23 +1,6 @@
-const express = require('express');
-const router = express.Router();
-const app = express();
 const fetch = require('node-fetch');
 
-var api_key = 'RGAPI-7f0faac7-6a67-4abe-b226-5753ad36d95c';
-var summoner_name = 'Derpiness'
-const URL = 'https://na1.api.riotgames.com/tft/summoner/v1/summoners/by-name/' + 'Derpiness' + '?api_key=' + api_key;
-
-
-
-const data = {
-    "id": "Nrw1MYkUFeA7Mj0mCPU7znxReL3Fj7tf9t5KNfqM6oDFM50",
-    "accountId": "fLGkB9JQz1ddN6lrWJL4AWekUY8bfnOJgrCcRXfM8QxYrw",
-    "puuid": "XCO8iXX3MFntUgNG3CHKeGwUAkCfM2YuxQ2N2tWeliM1BX_TDaKoVadReAp3HTwVb8JQm1pBa_MXVw",
-    "name": "Derpiness",
-    "profileIconId": 4658,
-    "revisionDate": 1606336124000,
-    "summonerLevel": 151
-}
+const api_key = 'RGAPI-76de0247-e288-4af3-a1dd-57e5585e1c68';
 
 function checkStatus(res){
     if(res.status == 200)
@@ -29,20 +12,26 @@ function checkStatus(res){
         throw error; 
     }
 }
-function getSummonerInfo()
-{
-    fetch(URL)
-    .then(checkStatus)
-    .then((res) =>res.json())
-    .then((res)=> json())
-    .then(json => {
-        this.test = json;
-        console.log(test);
-    })
-    .catch(function(error)
-    {
-        console.log(error);
-    }
 
-    )};
+exports.apiRequest = function (url, param)
+{
+    
+    const requestURL = url + param + '?api_key=' + api_key;
+
+    return fetch(requestURL)
+            //checks status
+            .then(checkStatus)
+            .then (function (response) 
+            {
+                return response.json();
+            })
+            .then (function (json) 
+            {
+                return json;
+            })
+            .catch(function(error)
+            {
+                console.log(error);
+            });
+};
     
