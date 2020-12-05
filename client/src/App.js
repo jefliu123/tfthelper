bfdf20eb1f89e6b3ad56cbea3661181673c5ac1c
Jeffrey Liu <jeffreyliu66@gmail.com>
2020-12-05 02:29:06 -0800

    moved search bar to header, changed some header styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,61 @@
 import React, { Component } from "react";
 import logo from "./Teamfight_Tactics_logo.svg";
 import "./App.css";
-
-import NameSearch from './nameSearch';
+import NameSearch from "./nameSearch";
+import AnalysisDisplay from "./analysisDisplay";
 
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <div className="App-title">Match History Analyst</div>
-                </header>
-                <NameSearch />
-            </div>
-        );
-    }
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			submittedUsername: "", //   last submitted text field
+			apiResponse: "", //         returned JSON from backend
+			showData: false, //         true after submit
+		};
+
+		this.setSubmittedUsername = this.setSubmittedUsername.bind(this);
+		this.setApiResponse = this.setApiResponse.bind(this);
+		this.setShowData = this.setShowData.bind(this);
+	}
+
+	setSubmittedUsername(sumbitted_username) {
+		this.setState({ submittedUsername: sumbitted_username });
+	}
+
+	setApiResponse(api_response) {
+		this.setState({ apiResponse: api_response });
+	}
+
+	setShowData(show_data) {
+		this.setState({ showData: show_data });
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<div className="App-top">
+						<div className="App-logoAndTitle">
+							<img src={logo} className="App-logo" alt="logo" />
+							<div className="App-title">MATCH HISTORY ANALYST</div>
+						</div>
+						<NameSearch
+							setSubmittedUsername={this.setSubmittedUsername}
+							setApiResponse={this.setApiResponse}
+							setShowData={this.setShowData}
+						/>
+					</div>
+				</header>
+				{this.state.showData && (
+					<AnalysisDisplay
+						username={this.state.submittedUsername}
+						parentResponse={this.state.apiResponse}
+					/>
+				)}
+			</div>
+		);
+	}
 }
 
 export default App;
--- client/src/nameSearch.js
+++ client/src/nameSearch.js
@@ -1,45 +1,70 @@
 import React, { Component } from "react";
 import "./App.css";
-import AnalysisDisplay from "./analysisDisplay";
 
 export default class NameSearch extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { currentUsername: "", submittedUsername: "", apiResponse: "", showData: false};
+	constructor(props) {
+		super(props);
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+		this.state = {
+			currentUsername: "", //current text field
+		};
 
-    handleChange(event) {
-        this.setState({currentUsername: event.target.value});
-    }
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
 
-    handleSubmit(event) {
-        event.preventDefault();
-        this.callAPI(this.state.currentUsername);
-        this.setState({submittedUsername: this.state.currentUsername});
-        this.setState({showData: true});
-        this.mainInput.value = "";
-    }
+	//called everytime text field changes
+	handleChange(event) {
+		//update currentUsername with current text in text field
+		this.setState({ currentUsername: event.target.value });
+	}
 
-    callAPI(summonerName) {
-        fetch("http://localhost:3006/carryHistory?summoner_name=" + summonerName)
-            .then(res => res.json())
-            .then(res => this.setState({ apiResponse: res}))
-            .catch(err => err);
-    }
+	// called on submit
+	handleSubmit(event) {
+		event.preventDefault();
+		// calls callAPI with entered username
+		this.callAPI(this.state.currentUsername);
+		// sets parent state submittedUsername to the submitted username
+		this.props.setSubmittedUsername(this.state.currentUsername);
+		// sets parent state showData to true (to display analysis)
+		this.props.setShowData(true);
+		// resets text field on submit
+		this.mainInput.value = "";
+	}
 
-    render() {
-        return (
-            <div className= "finder-body">
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-                <form className="submit-form" spellCheck="false" onSubmit={this.handleSubmit}>
-                    <input ref={(ref) => this.mainInput= ref} className= "finder-input" type="text" placeholder="Enter Summoner Name" value={this.state.value} onChange={this.handleChange} />
-                    <button className= "finder-submit" type="submit"><i className="fa fa-search"></i></button>
-                </form>
-                {this.state.showData && <AnalysisDisplay key = {this.state.submittedUsername} username = {this.state.submittedUsername} parentResponse = {this.state.apiResponse}/>}
-            </div>
-        );
-    }
+	// takes in summoner name and fetches JSON into apiResponse (in parent)
+	callAPI(summonerName) {
+		fetch("http://localhost:3006/carryHistory?summoner_name=" + summonerName)
+			.then((res) => res.json())
+			.then((res) => this.props.setApiResponse(res))
+			.catch((err) => err);
+	}
+
+	render() {
+		return (
+			<div className="finder-body">
+				<link
+					rel="stylesheet"
+					href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+				></link>
+				<form
+					className="submit-form"
+					spellCheck="false"
+					onSubmit={this.handleSubmit}
+				>
+					<input
+						ref={(ref) => (this.mainInput = ref)}
+						className="finder-input"
+						type="text"
+						placeholder="Enter Summoner Name"
+						value={this.state.value}
+						onChange={this.handleChange}
+					/>
+					<button className="finder-submit" type="submit">
+						<i className="fa fa-search"></i>
+					</button>
+				</form>
+			</div>
+		);
+	}
 }
